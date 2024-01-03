2caf0f0f87a1aebda3f2b38ee738d156fe452ac3
Felipe da Silva Martins <femartins2010@gmail.com>
2024-01-03 16:12:05 -0300

    adicionando o redux toolkit query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Provider } from 'react-redux'
 import { store } from './store'
 
-import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Produtos from './containers/Produtos'
 
@@ -15,20 +14,12 @@ export type Produto = {
 }
 
 function App() {
-  const [produtos, setProdutos] = useState<Produto[]>([])
-
-  useEffect(() => {
-    fetch('https://fake-api-tau.vercel.app/api/ebac_sports')
-      .then((res) => res.json())
-      .then((res) => setProdutos(res))
-  }, [])
-
   return (
     <Provider store={store}>
       <GlobalStyle />
       <div className="container">
         <Header />
-        <Produtos produtos={produtos} />
+        <Produtos />
       </div>
     </Provider>
   )
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import carrinhoReducer from './reducers/carrinho'
 import favoritoReducer from './reducers/favorito'
+import api from '../services/api'
 
 export const store = configureStore({
   reducer: {
     carrinho: carrinhoReducer,
-    favorito: favoritoReducer
-  }
+    favorito: favoritoReducer,
+    [api.reducerPath]: api.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware)
 })
 
 export type RootReducer = ReturnType<typeof store.getState>
